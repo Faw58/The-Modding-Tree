b0009c56f999e34e46a90706489a6d5f3c4cb325
Faw58 <fawwazarkan77@gmail.com>
2021-09-13 12:15:00 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,14 +13,17 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "A little update",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
+	<h3>v0.1</h3><br>
+		- Added few basic upgrades<br>
+		- Changed 'points' to 'numbers'<br>
+		- Changed 'Prestige' to 'Increment'<br>
 	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+		- Nothing as this is the first version<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
